4d373e5a4f3d40c76c2fd55b3dd5de7c1c339e08
Manex Agirrezabal <manex.agirrezabal@gmail.com>
2024-08-28 11:34:25 +0200

    Updated client, so that when it analyzes an empty line, it returns an empty line.

--- client.py
+++ client.py
@@ -1,9 +1,15 @@
 import requests
 import sys
 
+from requests.exceptions import ConnectionError
+
 server = "localhost"
 port = 8000
 
 payload = {'inp': sys.argv[1]}
-r = requests.get('http://'+server+':'+str(port)+'/get', params=payload)
-print (r.text)
+
+try:
+	r = requests.get('http://'+server+':'+str(port)+'/get', params=payload)
+	print (r.text)
+except ConnectionError:
+	print ()
